d2f08b7287b00cd755f4c4672bf5e781c4333b1a
julia <julsuniverse@gmail.com>
2018-09-27 17:02:36 +0300

    update routes

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,8 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function () {
+    return 'Hello World';
+});
+
 Route::post('/send_sms', 'DataController@get_data');
