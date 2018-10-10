b58722d39dfac67f75df1e960a712969e989f66b
julia <julsuniverse@gmail.com>
2018-10-10 18:27:59 +0300

    add header and footer to email report

---
+++ resources/views/emails/marketing/layout.blade.php
@@ -0,0 +1,15 @@
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <div style="border-bottom: 1px solid #999; background-color: #f5f5f5;">
+                <img src="{{ asset('images/logo.png') }}">
+            </div>
+
+           @yield('content')
+
+            <div style="text-align: center; border-top: 1px solid #999; background-color: #f5f5f5;">
+                <img src="{{ asset('images/logo.png') }}">
+            </div>
+        </div>
+    </div>
+</div>
